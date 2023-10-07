1108fef7cbeb1bc120f7248eea65f4a6d2b906af
Ribbit <echo07200503@gmail.com>
2023-10-07 13:53:49 +0900

    ♻ ローカルストレージの処理を共通化

--- src/lib/static.ts
+++ src/lib/static.ts
@@ -2,9 +2,10 @@ import manifest from '../../plugin/manifest.json';
 
 export const PLUGIN_NAME = manifest.name.ja;
 export const PLUGIN_VERSION = manifest.version;
-export const PLUGIN_KEY = 'kintone-plugin-toc';
+export const PLUGIN_KEY = 'toc';
 
-export const LOCAL_STORAGE_KEY = 'ribbit-kintone-plugin';
+export const LOCAL_STORAGE_PREFIX = 'ribbit-kintone-plugin-';
+export const LOCAL_STORAGE_KEY = `${LOCAL_STORAGE_PREFIX}${PLUGIN_KEY}`;
 
 export const URL_HOMEPAGE = manifest.homepage_url.ja;
 export const URL_INQUIRY = 'https://form.konomi.app';
