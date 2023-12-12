3f90953b8d1ecf496cef6f55ad8789316750e78b
Ribbit <echo07200503@gmail.com>
2023-12-12 09:25:47 +0900

    🔥 Remove unused URL_PLUGIN_LIST constant

--- src/lib/static.ts
+++ src/lib/static.ts
@@ -5,6 +5,5 @@ export const PLUGIN_NAME = manifest.name.ja;
 export const URL_INQUIRY = 'https://form.konomi.app';
 export const URL_PROMOTION = 'https://promotion.konomi.app/kintone-plugin';
 export const URL_BANNER = 'https://promotion.konomi.app/kintone-plugin/sidebar';
-export const URL_PLUGIN_LIST = 'https://ribbit.konomi.app/kintone-plugin';
 
 export const DEFAULT_COLOR = '#aaaaaa';
