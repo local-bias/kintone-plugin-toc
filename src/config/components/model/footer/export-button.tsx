d8c35f7ca86932d3f7baab42434b1c78b40b47f8
adachi <echo07200503@gmail.com>
2025-11-22 08:48:49 +0900

    ♻ recoil -> jotai

--- src/config/components/model/footer/export-button.tsx
+++ src/config/components/model/footer/export-button.tsx
@@ -1,43 +1,39 @@
-import React, { FC, memo, useState } from 'react';
-import { useRecoilCallback } from 'recoil';
 import { PLUGIN_NAME } from '@/lib/static';
-import { storageState } from '../../../states/plugin';
-import { useSnackbar } from 'notistack';
 import { PluginConfigExportButton } from '@konomi-app/kintone-utilities-react';
+import { useAtomValue } from 'jotai';
+import { useSnackbar } from 'notistack';
+import { FC, memo, useCallback, useState } from 'react';
+import { storageAtom } from '../../../states/plugin';
 
 const Component: FC = () => {
   const { enqueueSnackbar } = useSnackbar();
   const [loading, setLoading] = useState<boolean>(false);
+  const storage = useAtomValue(storageAtom);
 
-  const onClick = useRecoilCallback(
-    ({ snapshot }) =>
-      async () => {
-        try {
-          setLoading(true);
-          const storage = await snapshot.getPromise(storageState);
-          const blob = new Blob([JSON.stringify(storage, null)], {
-            type: 'application/json',
-          });
-          const url = (window.URL || window.webkitURL).createObjectURL(blob);
-          const link = document.createElement('a');
-          link.download = `${PLUGIN_NAME}-config.json`;
-          link.href = url;
-          document.body.appendChild(link);
-          link.click();
-          document.body.removeChild(link);
-          enqueueSnackbar('プラグインの設定情報をエクスポートしました', { variant: 'success' });
-        } catch (error) {
-          enqueueSnackbar(
-            'プラグインの設定情報のエクスポートに失敗しました。プラグイン開発者にお問い合わせください。',
-            { variant: 'error' }
-          );
-          throw error;
-        } finally {
-          setLoading(false);
-        }
-      },
-    []
-  );
+  const onClick = useCallback(async () => {
+    try {
+      setLoading(true);
+      const blob = new Blob([JSON.stringify(storage, null)], {
+        type: 'application/json',
+      });
+      const url = (window.URL || window.webkitURL).createObjectURL(blob);
+      const link = document.createElement('a');
+      link.download = `${PLUGIN_NAME}-config.json`;
+      link.href = url;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      enqueueSnackbar('プラグインの設定情報をエクスポートしました', { variant: 'success' });
+    } catch (error) {
+      enqueueSnackbar(
+        'プラグインの設定情報のエクスポートに失敗しました。プラグイン開発者にお問い合わせください。',
+        { variant: 'error' }
+      );
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  }, [storage, enqueueSnackbar]);
 
   return <PluginConfigExportButton loading={loading} onExportButtonClick={onClick} />;
 };
