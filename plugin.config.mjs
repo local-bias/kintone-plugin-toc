71e57e89c84c0a07c763a8d6ae5e7fb094559737
adachi <echo07200503@gmail.com>
2025-11-22 08:52:54 +0900

    ♻ 設定ファイルの修正: ビルド設定を無効化し、翻訳を追加

--- plugin.config.mjs
+++ plugin.config.mjs
@@ -11,14 +11,7 @@ export default /** @type { const } */ ({
     port: 57590,
   },
   lint: {
-    build: true,
-  },
-  tailwind: {
-    css: 'src/styles/global.css',
-    config: {
-      desktop: 'tailwind.config.desktop.mjs',
-      config: 'tailwind.config.config.mjs',
-    },
+    build: false,
   },
   manifest: {
     base: {
@@ -29,20 +22,29 @@ export default /** @type { const } */ ({
         en: 'Table of Contents Plugin',
         ja: '目次プラグイン',
         zh: '目录插件',
+        'zh-TW': '目錄外掛程式',
+        es: 'Plugin de tabla de contenidos',
+        'pt-BR': 'Plugin de tabela de conteúdos',
+        th: 'ปลั๊กอินสารบัญ',
       },
       description: {
         en: 'Add a table of contents to the record editing screen and enable scrolling to any section.',
         ja: 'レコード編集画面に目次を追加し、任意のセクションへスクロールできるようにします',
         zh: '在记录编辑界面添加目录，并实现可以滚动到任意部分。',
+        'zh-TW': '在記錄編輯畫面添加目錄，並實現可以捲動到任意部分。',
+        es: 'Agrega una tabla de contenidos a la pantalla de edición de registros y permite desplazarse a cualquier sección.',
+        'pt-BR':
+          'Adiciona uma tabela de conteúdos ao ecrã de edição de registos e permite deslocar-se para qualquer secção.',
+        th: 'เพิ่มสารบัญลงในหน้าจอแก้ไขระเบียนและเปิดใช้งานการเลื่อนไปยังส่วนใดก็ได้',
       },
       icon: 'icon.png',
       homepage_url: { ja: hp, en: hp },
-      desktop: { js: [`${cdn}/common/desktop.js`], css: [] },
-      mobile: { js: [`${cdn}/common/desktop.js`], css: [] },
+      desktop: { js: [`${cdn}/common/desktop.js`], css: [`${cdn}/common/desktop.css`] },
+      mobile: { js: [`${cdn}/common/desktop.js`], css: [`${cdn}/common/desktop.css`] },
       config: {
         html: 'config.html',
         js: [`${cdn}/common/config.js`],
-        css: [],
+        css: [`${cdn}/common/config.css`],
         required_params: [],
       },
     },
