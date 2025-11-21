685c4d304b67e659a8f43d26d8b038a30ce040f7
adachi <echo07200503@gmail.com>
2025-11-22 08:48:38 +0900

    ♻ recoil -> jotai

--- src/config/app.tsx
+++ src/config/app.tsx
@@ -1,40 +1,39 @@
-import { PluginErrorBoundary } from '@/lib/components/error-boundary';
+import { ErrorBoundary } from '@/lib/components/error-boundary';
 import { URL_BANNER, URL_PROMOTION } from '@/lib/static';
 import {
+  Notification,
   PluginBanner,
+  PluginConfigProvider,
   PluginContent,
   PluginLayout,
-  PluginConfigProvider,
-  Notification,
 } from '@konomi-app/kintone-utilities-react';
 import { LoaderWithLabel } from '@konomi-app/ui-react';
 import { SnackbarProvider } from 'notistack';
-import React, { FC, Suspense } from 'react';
-import { RecoilRoot } from 'recoil';
+import { FC, Suspense } from 'react';
+import config from '../../plugin.config.mjs';
 import Footer from './components/model/footer';
 import Form from './components/model/form';
-import config from '../../plugin.config.mjs';
 
 const App: FC = () => (
   <Suspense fallback={<LoaderWithLabel label='画面の描画を待機しています' />}>
-    <RecoilRoot>
-      <PluginErrorBoundary>
-        <PluginConfigProvider config={config}>
-          <Notification />
-          <SnackbarProvider maxSnack={1}>
-            <Suspense fallback={<LoaderWithLabel label='設定情報を取得しています' />}>
-              <PluginLayout singleCondition>
-                <PluginContent>
+    <ErrorBoundary config={config}>
+      <PluginConfigProvider config={config}>
+        <Notification />
+        <SnackbarProvider maxSnack={1}>
+          <Suspense fallback={<LoaderWithLabel label='設定情報を取得しています' />}>
+            <PluginLayout singleCondition>
+              <PluginContent>
+                <ErrorBoundary config={config}>
                   <Form />
-                </PluginContent>
-                <PluginBanner url={URL_BANNER} />
-                <Footer />
-              </PluginLayout>
-            </Suspense>
-          </SnackbarProvider>
-        </PluginConfigProvider>
-      </PluginErrorBoundary>
-    </RecoilRoot>
+                </ErrorBoundary>
+              </PluginContent>
+              <PluginBanner url={URL_BANNER} />
+              <Footer />
+            </PluginLayout>
+          </Suspense>
+        </SnackbarProvider>
+      </PluginConfigProvider>
+    </ErrorBoundary>
     <iframe
       title='promotion'
       loading='lazy'
